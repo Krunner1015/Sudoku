d152993a99ba2fb4f6007d02e255af8f794333c5
milesbryan-prog <miles.bryan@ufl.edu>
2024-11-18 13:14:21 -0500

    Update Sudoku.py

--- Sudoku.py
+++ Sudoku.py
@@ -1,61 +1,61 @@
 import pygame
 BG_COLOR = (173, 216, 230)
-screen = pygame.display.set_mode((720, 720))
+screen = pygame.display.set_mode((405, 504))
 
 def drawLines():
-        for i in range(0, 3):
-            pygame.draw.line(screen, "black", (0, i * 240), (720, i * 240), 5)
-            pygame.draw.line(screen, "black", (i * 240, 0), (i * 240, 720), 5)
+        for i in range(0, 4):
+            pygame.draw.line(screen, "black", (0, i * 135), (405, i * 135), 5)
+            pygame.draw.line(screen, "black", (i * 135, 0), (i * 135, 405), 5)
             for i in range(0, 9):
-                pygame.draw.line(screen, "black", (0, i * 80), (720, i * 80), 2)
+                pygame.draw.line(screen, "black", (0, i * 45), (405, i * 45), 2)
             for i in range(0, 9):
-                pygame.draw.line(screen, "black", (i * 80, 0), (i * 80, 720), 2)
+                pygame.draw.line(screen, "black", (i * 45, 0), (i * 45, 405), 2)
 
 def opening_Screen():
             background_image = pygame.image.load("background_Soduko.jpg")
-            screen.blit(background_image, background_image.get_rect(center =(500, 350)))
+            screen.blit(background_image, background_image.get_rect(center =(202.5, 252)))
             welcome = "Welcome to Sudoku"
-            intro = pygame.font.Font(None, 100)
+            intro = pygame.font.Font(None, 60)
             welcome_surf = intro.render(welcome, 0, "black")
-            welcome_rect = welcome_surf.get_rect(center=(375, 200))
+            welcome_rect = welcome_surf.get_rect(center=(202.5, 100))
             screen.blit(welcome_surf, welcome_rect)
             options = "Select Game Mode"
-            options_1 = pygame.font.Font(None, 50)
+            options_1 = pygame.font.Font(None, 40)
             options_surf = options_1.render(options, 0, "black")
-            options_rect = options_surf.get_rect(center=(375, 400))
+            options_rect = options_surf.get_rect(center=(202.5, 175))
             screen.blit(options_surf, options_rect)
-            difficulty = pygame.font.Font(None, 40)
+            difficulty = pygame.font.Font(None, 25)
             easy_text = "Easy"
             medium_text = "Medium"
             hard_text = "Hard"
-            pygame.draw.line(screen, "orange", (80, 480), (240, 480), 3)
-            pygame.draw.line(screen, "orange", (80, 480), (80, 560), 3)
-            pygame.draw.line(screen, "orange", (80, 560), (240, 560), 3)
-            pygame.draw.line(screen, "orange", (240, 480), (240, 560), 3)
+            pygame.draw.line(screen, "orange", (30, 352), (120, 352), 3)
+            pygame.draw.line(screen, "orange", (30, 352), (30, 402), 3)
+            pygame.draw.line(screen, "orange", (30, 402), (120, 402), 3)
+            pygame.draw.line(screen, "orange", (120, 352), (120, 402), 3)
             easy_surf = difficulty.render(easy_text, 0, "white")
-            end_rect = easy_surf.get_rect(center=(159, 515))
+            end_rect = easy_surf.get_rect(center=(75, 377))
             screen.blit(easy_surf, end_rect)
 
-            pygame.draw.line(screen, "orange", (320, 480), (480, 480), 3)
-            pygame.draw.line(screen, "orange", (320, 480), (320, 560), 3)
-            pygame.draw.line(screen, "orange", (320, 560), (480, 560), 3)
-            pygame.draw.line(screen, "orange", (480, 480), (480, 560), 3)
+            pygame.draw.line(screen, "orange", (155, 352), (245, 352), 3)
+            pygame.draw.line(screen, "orange", (155, 352), (155, 402), 3)
+            pygame.draw.line(screen, "orange", (155, 402), (245, 402), 3)
+            pygame.draw.line(screen, "orange", (245, 352), (245, 402), 3)
             medium_surf = difficulty.render(medium_text, 0, "white")
-            end_rect = medium_surf.get_rect(center=(401, 515))
+            end_rect = medium_surf.get_rect(center=(200, 377))
             screen.blit(medium_surf, end_rect)
 
-            pygame.draw.line(screen, "orange", (560, 480), (720, 480), 3)
-            pygame.draw.line(screen, "orange", (560, 480), (560, 560), 3)
-            pygame.draw.line(screen, "orange", (560, 560), (720, 560), 3)
-            pygame.draw.line(screen, "orange", (720, 480), (720, 560), 3)
+            pygame.draw.line(screen, "orange", (280, 352), (370, 352), 3)
+            pygame.draw.line(screen, "orange", (280, 352), (280, 402), 3)
+            pygame.draw.line(screen, "orange", (280, 402), (370, 402), 3)
+            pygame.draw.line(screen, "orange", (370, 352), (370, 402), 3)
             hard_surf = difficulty.render(hard_text, 0, "white")
-            end_rect = hard_surf.get_rect(center=(643, 515))
+            end_rect = hard_surf.get_rect(center=(325, 377))
             screen.blit(hard_surf, end_rect)
 
 
 try:
     pygame.init()
-    screen = pygame.display.set_mode((720, 720))
+    screen = pygame.display.set_mode((405, 504))
     running = True
     while running:
         screen.fill("light blue")
