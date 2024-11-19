fbb57e8bb727c85629e2ef755dd849552c54c1dd
SivanRP <sivanreddypushpagiri@gmail.com>
2024-11-19 18:11:20 -0500

    Changed Code

--- cell.py
+++ cell.py
@@ -1,21 +1,25 @@
-chip_font = 400
+import pygame
 class Cell:
-    def __init__(self, value, row, col, screen):
+    def __init__(self,value,row,col,screen):
         self.value = value
+        self.sketched_value = 0
         self.row = row
         self.col = col
         self.screen = screen
-
-    def set_cell_value(self, value):
-        set.value = value
-
-    def set_sketched_value(self, value):
+    
+    def set_cell_value(self,value):
+        self.value = value
+    
+    def set_sketched_value(self,value):
         self.sketched_value = value
-
+    
     def draw(self):
-        if 0 < self.value <= 9:
-            chip_value = chip_font.render(str(self.value),0,(66,66,66))
-            for row in range(9):
-                for col in range(9):
-                    chip_value.get_rect(center = (col * 80 + 40, row * 80 + 40))
-##flag run = - 
+        font = pygame.font.SysFont("Arial",40)
+        if self.value != 0:
+            text = font.render(str(self.value),True,(0,0,0))
+            self.screen.blit(text, (self.col * 60 +20, self.row * 60 + 10))
+        elif self.sketched_value != 0:
+            text = font.render(str(self.sketched_value),True,(128,128,128))
+            self.screen.blit(text, (self.col * 60 +5, self.row * 60 + 5))
+        else:
+            pygame.draw.rect(self.screen,(255,0,0), (self.col * 60, self.row * 60,60,60),3)
