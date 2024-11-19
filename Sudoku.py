46157605ea73f1ff11c41e23107044deb35e44ee
Krunner1015 <kbartrum@ufl.edu>
2024-11-18 22:30:18 -0500

    Updated

--- Sudoku.py
+++ Sudoku.py
@@ -1,6 +1,14 @@
-import pygame
+import pygame, sys
+from sudoku_generator import *
+
 BG_COLOR = (173, 216, 230)
+
+pygame.init()
 screen = pygame.display.set_mode((405, 504))
+pygame.display.set_caption("Sudoku")
+pygame.display.flip()
+
+game_over = False
 
 def drawLines():
         for i in range(0, 4):
@@ -52,16 +60,98 @@ def opening_Screen():
             end_rect = hard_surf.get_rect(center=(325, 377))
             screen.blit(hard_surf, end_rect)
 
+def in_game():
+    button_font = pygame.font.Font(None, 25)
 
-try:
-    pygame.init()
-    screen = pygame.display.set_mode((405, 504))
-    running = True
-    while running:
-        screen.fill("light blue")
-        drawLines()
-        opening_Screen()
-        pygame.display.flip()
-finally:
-    pygame.quit()
+    pygame.draw.line(screen, "orange", (30, 454), (120, 454), 3)
+    pygame.draw.line(screen, "orange", (30, 454), (30, 504), 3)
+    pygame.draw.line(screen, "orange", (30, 504), (120, 504), 3)
+    pygame.draw.line(screen, "orange", (120, 454), (120, 504), 3)
+    button1 = "Reset"
+    button1_surf = button_font.render(button1, 0, "white")
+    end_rect = button1_surf.get_rect(center=(75, 479))
+    screen.blit(button1_surf, end_rect)
 
+    pygame.draw.line(screen, "orange", (155, 454), (245, 454), 3)
+    pygame.draw.line(screen, "orange", (155, 454), (155, 504), 3)
+    pygame.draw.line(screen, "orange", (155, 504), (245, 504), 3)
+    pygame.draw.line(screen, "orange", (245, 454), (245, 504), 3)
+    button2 = "Restart"
+    button2_surf = button_font.render(button2, 0, "white")
+    end_rect = button2_surf.get_rect(center=(200, 479))
+    screen.blit(button2_surf, end_rect)
+
+    pygame.draw.line(screen, "orange", (280, 454), (370, 454), 3)
+    pygame.draw.line(screen, "orange", (280, 454), (280, 504), 3)
+    pygame.draw.line(screen, "orange", (280, 504), (370, 504), 3)
+    pygame.draw.line(screen, "orange", (370, 454), (370, 504), 3)
+    button3 = "Exit"
+    button3_surf = button_font.render(button3, 0, "white")
+    end_rect = button3_surf.get_rect(center=(325, 479))
+    screen.blit(button3_surf, end_rect)
+
+def victory_screen():
+    button_font = pygame.font.Font(None, 25)
+    background_image = pygame.image.load("background_Soduko.jpg")
+    screen.blit(background_image, background_image.get_rect(center=(202.5, 252)))
+    victory_text = "Game Won!"
+    victory_message = pygame.font.Font(None, 60)
+    victory_surf = victory_message.render(victory_text, 0, "black")
+    victory_rect = victory_surf.get_rect(center=(202.5, 100))
+    screen.blit(victory_surf, victory_rect)
+    pygame.draw.line(screen, "orange", (280, 454), (370, 454), 3)
+    pygame.draw.line(screen, "orange", (280, 454), (280, 504), 3)
+    pygame.draw.line(screen, "orange", (280, 504), (370, 504), 3)
+    pygame.draw.line(screen, "orange", (370, 454), (370, 504), 3)
+    button3 = "Exit"
+    button3_surf = button_font.render(button3, 0, "white")
+    end_rect = button3_surf.get_rect(center=(202.5, 300))
+    screen.blit(button3_surf, end_rect)
+
+def loss_screen():
+    button_font = pygame.font.Font(None, 25)
+    background_image = pygame.image.load("background_Soduko.jpg")
+    screen.blit(background_image, background_image.get_rect(center=(202.5, 252)))
+    victory_text = "Game Over :("
+    victory_message = pygame.font.Font(None, 60)
+    victory_surf = victory_message.render(victory_text, 0, "black")
+    victory_rect = victory_surf.get_rect(center=(202.5, 100))
+    screen.blit(victory_surf, victory_rect)
+    pygame.draw.line(screen, "orange", (280, 454), (370, 454), 3)
+    pygame.draw.line(screen, "orange", (280, 454), (280, 504), 3)
+    pygame.draw.line(screen, "orange", (280, 504), (370, 504), 3)
+    pygame.draw.line(screen, "orange", (370, 454), (370, 504), 3)
+    button3 = "Restart"
+    button3_surf = button_font.render(button3, 0, "white")
+    end_rect = button3_surf.get_rect(center=(202.5, 300))
+    screen.blit(button3_surf, end_rect)
+
+def difficulty_selection(click_x, click_y):
+    easy = (30, 352, 120, 402)
+    medium = (155, 352, 245, 402)
+    hard = (280, 352, 370, 402)
+
+    if easy[0] < click_x < easy[2] and easy[1] < click_y < easy[3]:
+        return "easy"
+
+    if medium[0] < click_x < medium[2] and medium[1] < click_y < medium[3]:
+        return "medium"
+
+    if hard[0] < click_x < hard[2] and hard[1] < click_y < hard[3]:
+        return "hard"
+
+opening_Screen()
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+
+        if event.type == pygame.MOUSEBUTTONDOWN and not game_over:
+            x, y = event.pos()
+            difficulty = difficulty_selection(x, y)
+            screen.fill("light blue")
+            drawLines()
+            in_game()
+            pygame.display.flip()
