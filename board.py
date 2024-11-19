2996cf539cbc18edc92da905d9a0d788813e182e
jeremiahhidayat <Jeremiahhidayat@yahoo.com>
2024-11-19 16:45:23 -0500

    Updated Board Class with Functioning Select Square

--- board.py
+++ board.py
@@ -1,3 +1,6 @@
+import pygame
+import sys
+
 class Board:
     def __init__(self, width, height, screen, difficulty):
         self.width = width
@@ -5,35 +8,118 @@ class Board:
         self.screen = screen
         self.difficulty = difficulty
 
+
     def draw(self):
-        pass
+        # Draws an outline of the Sudoku grid, with bold lines to delineate the 3x3 boxes.
+        # Draws every cell on this board.
+        for i in range(0, 4):
+                pygame.draw.line(self.screen, "black", (0, i * 135), (405, i * 135), 4)
+                pygame.draw.line(self.screen, "black", (i * 135, 0), (i * 135, 405), 4)
+                for i in range(0, 9):
+                    pygame.draw.line(self.screen, "black", (0, i * 45), (405, i * 45), 2)
+                for i in range(0, 9):
+                    pygame.draw.line(self.screen, "black", (i * 45, 0), (i * 45, 405), 2)
 
     def select(self, row, col):
-        pass
+        # Marks the cell at (row, col) in the board as the current selected cell.
+        # Once a cell has been selected, the user can edit its value or sketched value.
+        pygame.draw.rect(screen, "red", pygame.Rect(col*self.width/9, row*self.height/9, 2, 47))
+        pygame.draw.rect(screen, "red", pygame.Rect(col*self.width/9, row*self.height/9, 47,2))
+        pygame.draw.rect(screen, "red", pygame.Rect(self.width/9+col*self.width/9, row*self.height/9, 2, 47))
+        pygame.draw.rect(screen, "red", pygame.Rect(col*self.width/9, self.height/9+row*self.height/9, 47,2))
+
+
+        
 
     def click(self, row, col):
+    # 	If a tuple of (x,y) coordinates is within the displayed board, 
+    # this function returns a tuple of the (row, col) of the cell which was clicked. 
+    # Otherwise, this function returns None.
         pass
 
     def clear(self):
+    # 	Clears the value cell. 
+    # Note that the user can only remove the cell values and 
+    # sketched values that are filled by themselves.
         pass
 
     def sketch(self, value):
+        # Sets the sketched value of the current selected cell equal to the user entered value.
+        # It will be displayed at the top left corner of the cell using the draw() function.
         pass
 
     def place_number(self, value):
+        # Sets the value of the current selected cell equal to the user entered value. 
+        # Called when the user presses the Enter key.
         pass
 
     def reset_to_original(self):
+        # Resets all cells in the board to their original values 
+        # (0 if cleared, otherwise the corresponding digit).
         pass
 
     def is_full(self):
+        # Returns a Boolean value indicating whether the board is full or not.
         pass
 
     def update_board(self):
+        # Updates the underlying 2D board with the values in all cells.
         pass
 
     def find_empty(self):
+        # Finds an empty cell and returns its row and col as a tuple (x,y).
         pass
 
     def check_board(self):
+        # Check whether the Sudoku board is solved correctly.
         pass
+
+
+pygame.init()
+pygame.key.set_repeat(1000,0) 
+screen = pygame.display.set_mode((405, 504))
+board = Board(405,405,screen,"easy")
+row = 9
+col = 9
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            (x,y) = event.pos
+            row = y//45
+            col = x//45
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_UP:
+                row-=1
+                print("Up")
+                print(row)
+            if event.key == pygame.K_DOWN:
+                row+=1
+                print("Down")
+                print(row)
+            if event.key == pygame.K_RIGHT:
+                col +=1
+                print("Right")
+                print(col)
+            if event.key == pygame.K_LEFT:
+                col -=1
+                print("Left")
+                print(col)
+    screen.fill("light blue")
+    board.draw()
+
+    # Code for select function
+
+    if row > 8:
+        row = 8
+    if col > 8:
+        col = 8
+    if row < 0:
+        row = 0
+    if col < 0:
+        col =0
+
+    board.select(row,col)
+    pygame.display.flip()
